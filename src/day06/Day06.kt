88bbf39ed619c228f3a2da351a60020d5d0d3f2a
Terry Zeng <zeng.terry.biz@gmail.com>
2023-12-11 00:13:41 -0800

    Day06 solution improvement: Use quadratic equation

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -2,6 +2,7 @@ package day06
 
 import println
 import readInput
+import kotlin.math.sqrt
 
 const val DIR = "Day06"
 
@@ -23,7 +24,7 @@ class Part1(private val input: List<String>) {
             input.first().substringAfter("Time:").split("  ").filter { it.isNotBlank() }.map { it.trim().toLong() }
         val distances =
             input.last().substringAfter("Distance:").trim().split("  ").filter { it.isNotBlank() }
-                .map { it.trim().toLong() }
+                    .map { it.trim().toLong() }
 
         return times.zip(distances) { time, distance -> RaceRecord(time, distance) }
     }
@@ -31,19 +32,25 @@ class Part1(private val input: List<String>) {
 
 class Part2(private val input: List<String>) {
     fun solve(): Int {
-        return parseRaceRecord(input).also { it.println() }.betterCount()
+        return parseRaceRecord(input).betterCount()
     }
 
     private fun parseRaceRecord(input: List<String>) = RaceRecord(
-        input.first().filter { it.isDigit() }.toLong(),
-        input.last().filter { it.isDigit() }.toLong()
-    )
+            input.first().filter { it.isDigit() }.toLong(),
+            input.last().filter { it.isDigit() }.toLong()
+                                                                 )
 }
 
 private data class RaceRecord(val time: Long, val distance: Long) {
     fun betterCount(): Int {
-        return (1..time).mapNotNull {
-            if (it * (time - it) > distance) it else null
-        }.count()
+        // For the record distance, its time t satisfies t^2 - time * t + distance = 0.
+        // We use the smaller of the two roots as the time use to produce the distance
+        val t1 = ((time - sqrt(time * time - 4.0 * distance)) / 2.0).toInt()
+
+        // The longest distance is produced by time/2 (tip).
+        // Each second between (t1, tip] and [tip, t2) will produce a better distance.
+        // Note that tip only counts for one better distance if time is an even number.
+        val offset = if (time % 2 == 0L) 1 else 0
+        return ((time / 2 - t1) * 2 - offset).toInt()
     }
 }
